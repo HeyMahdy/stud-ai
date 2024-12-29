5bf547da2778c76ec952b466fa315ef6c31c0194
Mahdy Muzzammil <156871399+HeyMahdy@users.noreply.github.com>
2024-12-29 15:14:26 +0600

    cros addded . working or local not for docker

--- app/hello.py
+++ app/hello.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 from Models.testModel2 import Comment , CommentCreate
 
 from DatabaseConfiguration.databaseConfig import SessionDep
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
